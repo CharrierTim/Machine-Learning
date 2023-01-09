4827668e4f82c1d0a512fad67ae019d04784477d
CharrierTim <timothee.charrier@bbox.fr>
2023-01-09 12:59:29 +0100

    Co-authored-by: raveletthomas <raveletthomas@users.noreply.github.com>

---
+++ TD1/Conv.py
@@ -0,0 +1,16 @@
+# Convolutional Neural Network
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.utils import np_utils
+
+# Importing the dataset
+from keras.datasets import mnist
+
+import tensorflow as tf
+
+from keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
